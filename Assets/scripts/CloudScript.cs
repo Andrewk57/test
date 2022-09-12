570b9cbbf98b2f4443842c16ceb30c5132551cfe
Andrewk57 <112024432+Andrewk57@users.noreply.github.com>
2022-09-12 16:25:39 -0400

    Added assets

---
+++ Assets/scripts/CloudScript.cs
@@ -0,0 +1,23 @@
+//Libraries
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The main class of the script
+public class CloudScript : MonoBehaviour
+{
+    public float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(this.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Frame");
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + (speed * Time.deltaTime));
+        //Debug.Log(speed * Time.deltaTime);
+    }
+}
